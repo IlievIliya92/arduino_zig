46cef6d4b690f30d3d47f4290b3c0b02b8ebcf30
Iliya Iliev <iliyailiev3592@gmail.com>
2020-10-25 12:21:12 +0200

    atmega328p: Add enum to define portb & asm interface to enb/disbl isr
    Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

--- src/atmega328p.zig
+++ src/atmega328p.zig
@@ -1,3 +1,38 @@
 // --- ATMEGA328p port ---
+
+// --- ENUMS ---
+pub const PORTB = enum(u3) {
+    B0,
+    B1,
+    B2,
+    B3,
+    B4,
+    B5,
+    B6,
+    B7,
+};
+
+// --- Register Mapping
+pub const pinb = @intToPtr(*volatile u8, 0x23);
 pub const ddrb = @intToPtr(*volatile u8, 0x24);
 pub const portb = @intToPtr(*volatile u8, 0x25);
+
+// --- ASM
+// Global Interrupt Enable
+pub fn sei() void {
+    asm volatile ("sei"
+        :
+        :
+        : "memory"
+    );
+}
+
+// Global Interrupt Disable
+pub fn cli() void {
+    asm volatile ("cli"
+        :
+        :
+        : "memory"
+    );
+}
+
