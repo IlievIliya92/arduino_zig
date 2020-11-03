3c8e2326ffb8f57f463b9d0529e64173108004cc
Iliya Iliev <iliyailiev3592@gmail.com>
2020-11-03 09:54:34 +0200

    atmega328: Add serial comm registers map
    Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

--- src/atmega328p.zig
+++ src/atmega328p.zig
@@ -1,8 +1,13 @@
 // --- ATMEGA328p port ---
 
+// --- Constants
+// FOSC (16MHz)
+pub const CPU_CLOCK_HZ = 16000000;
+
 // --- ENUMS ---
 
-// --- PORTB MAP ---
+
+// --- Register Mapping
 pub const PORTB = enum(u3) {
     B0,
     B1,
@@ -13,23 +18,58 @@ pub const PORTB = enum(u3) {
     B6,
     B7,
 };
-
-// --- COMM INTERFACES ---
-pub const eCOMPort = enum(u3) {
-    USART0,
-    MSPIM0,
-    USART1,
-    MSPIM1,
-    USART2,
-    MSPIM2,
-    USART3,
-    MSPIM3,
-};
-
-// --- Register Mapping
 pub const pinb = @intToPtr(*volatile u8, 0x23);
 pub const ddrb = @intToPtr(*volatile u8, 0x24);
 pub const portb = @intToPtr(*volatile u8, 0x25);
+
+
+// --- Serial Communication
+pub const UCSR0A = enum(u3) {
+    MPCM0,
+    U2X0,
+    UPE0,
+    DOR0,
+    FE0,
+    UDRE0,
+    TXC0,
+    RXC0,
+};
+pub const ucsr0a = @intToPtr(*volatile u8, 0xC0);
+
+pub const UCSR0B = enum(u3) {
+    TXB8,
+    RXB80,
+    UCSZ02,
+    TXEN0,
+    RXEN0,
+    UDRIE0,
+    TXCIE,
+    RXCIE0,
+};
+pub const ucsr0b = @intToPtr(*volatile u8, 0xC1);
+
+pub const UCSR0C = enum(u3) {
+    UCPOL0,
+    UCSZ00,
+    UCSZ01,
+    USBS0,
+    UPM00,
+    UPM01,
+    UMSEL00,
+    UMSEL01,
+};
+pub const ucsr0c = @intToPtr(*volatile u8, 0xC2);
+
+pub const ubrr0l = @intToPtr(*volatile u8, 0xC4);
+pub const ubrr0h = @intToPtr(*volatile u8, 0xC5);
+pub const udr0 = @intToPtr(*volatile u8, 0xC6);
+
+
+// Timer
+pub const tcnt1 = @intToPtr(*volatile u16, 0x84);
+pub const tccr1a = @intToPtr(*volatile u8, 0x80);
+pub const tccr1b = @intToPtr(*volatile u8, 0x81);
+pub const timsk1 = @intToPtr(*volatile u8, 0x6f);
 
 // --- ASM port
 
@@ -53,14 +93,14 @@ pub fn DISABLE_INTERRUPTS() void {
 }
 
 pub fn ENTER_CRITICAL() void {
-    asm volatile ("in __tmp_reg__, __SREG__");
+    //asm volatile ("in __tmp_reg__, __SREG__");
     DISABLE_INTERRUPTS();
-    asm volatile ("push __tmp_reg__");
+    //asm volatile ("push __tmp_reg__");
     BARRIER();
 }
 
 pub fn EXIT_CRITICAL() void {
-    asm volatile ("pop __tmp_reg__");
-    asm volatile ("out __SREG__, __tmp_reg__");
+    //asm volatile ("pop __tmp_reg__");
+    //asm volatile ("out __SREG__, __tmp_reg__");
     BARRIER();
 }
